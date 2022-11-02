59195b20d7eabeab098b9713a1690a5f3a887740
leonardo <leonardotobe94@gmail.com>
2022-11-02 16:37:47 -0400

    add ambassador img

--- const/machines.js
+++ const/machines.js
@@ -1,65 +1,65 @@
 export const MACHINES = [
   [
-    "Late", {
-    name: 'Late',
-    description: 'SSTI (Server Side Template Injection) y privilegios "cron".',
-    posted: '10-08-2022',
-    src: '/late_info.webp',
-    alt: 'Late Hack The Box'
+    'Late', {
+      name: 'Late',
+      description: 'SSTI (Server Side Template Injection) y privilegios "cron".',
+      posted: '10-08-2022',
+      src: '/late_info.webp',
+      alt: 'Late Hack The Box'
     }
   ],
   [
-    "EarlyAccess", {
-    name: 'EarlyAccess',
-    description: 'Favorita.',
-    posted: '17-08-2022',
-    src: '/earlyAccess_info.webp',
-    alt: 'Máquina Late en Hack The Box una de las mas faciles'
+    'EarlyAccess', {
+      name: 'EarlyAccess',
+      description: 'Favorita.',
+      posted: '17-08-2022',
+      src: '/earlyAccess_info.webp',
+      alt: 'Máquina Late en Hack The Box una de las mas faciles'
     }
   ],
   [
-    "Cap", {
-    name: 'Cap',
-    posted: "24-08-2022",
-    src: '/cap/Cap.webp',
-    description: 'Capability SET-SUID y Tshark',
-    alt: 'Máquina Cap'
+    'Cap', {
+      name: 'Cap',
+      posted: '24-08-2022',
+      src: '/cap/Cap.webp',
+      description: 'Capability SET-SUID y Tshark',
+      alt: 'Máquina Cap'
     }
   ],
   [
-    "Trick", {
-    name: 'Trick',
-    description: 'Servicio SMTP con la herramienta dig, subdominios con ffuf y sqlmap, root con fail2ban.',
-    posted: "31-08-2022",
-    src: 'https://res.cloudinary.com/djc1umong/image/upload/v1663697041/Trick_1_gawv9i.webp',
-    alt: 'Máquina Trick'
+    'Trick', {
+      name: 'Trick',
+      description: 'Servicio SMTP con la herramienta dig, subdominios con ffuf y sqlmap, root con fail2ban.',
+      posted: '31-08-2022',
+      src: 'https://res.cloudinary.com/djc1umong/image/upload/v1663697041/Trick_1_gawv9i.webp',
+      alt: 'Máquina Trick'
     }
   ],
   [
-    "Shoppy", {
-    name: 'Shoppy',
-    description: 'Montaremos con Docker sin antes Fuzzear e Inyectar SQL.',
-    posted: "22-09-2022",
-    src: 'https://res.cloudinary.com/djc1umong/image/upload/v1663907495/Shoppy_1_gfaxxf.webp',
-    alt: 'Máquina Shoppy'
+    'Shoppy', {
+      name: 'Shoppy',
+      description: 'Montaremos con Docker sin antes Fuzzear e Inyectar SQL.',
+      posted: '22-09-2022',
+      src: 'https://res.cloudinary.com/djc1umong/image/upload/v1663907495/Shoppy_1_gfaxxf.webp',
+      alt: 'Máquina Shoppy'
     }
   ],
   [
-    "Photobomb", {
-    name: 'Photobomb',
-    description: 'Burpsuite.',
-    posted: "14-10-2022",
-    src: 'https://res.cloudinary.com/djc1umong/image/upload/v1665956516/Photobomb_znmv8y.webp',
-    alt: 'Photobomb'
+    'Photobomb', {
+      name: 'Photobomb',
+      description: 'Burpsuite.',
+      posted: '14-10-2022',
+      src: 'https://res.cloudinary.com/djc1umong/image/upload/v1665956516/Photobomb_znmv8y.webp',
+      alt: 'Photobomb'
     }
   ],
   [
-    "Ambassador", {
-    name: 'Ambassador',
-    description: 'Ambassador',
-    posted: "21-10-2022",
-    src: 'https://res.cloudinary.com/djc1umong/image/upload/v1665956516/Photobomb_znmv8y.webp',
-    alt: 'Ambassador',
+    'Ambassador', {
+      name: 'Ambassador',
+      description: 'Ambassador',
+      posted: '21-10-2022',
+      src: 'https://res.cloudinary.com/djc1umong/image/upload/v1667435625/Ambassador_ufms7x.webp',
+      alt: 'Ambassador'
     }
   ]
 ]
