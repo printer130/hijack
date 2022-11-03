9ab2c7c100cd00ab63331e134972ead420593892
leonardo <leonardotobe94@gmail.com>
2022-11-03 11:28:26 -0400

    img ambassador

--- pages/box/Ambassador.js
+++ pages/box/Ambassador.js
@@ -1,12 +1,14 @@
-import { DynamicHero } from "../../Container/DynamicHero"
-import { Ambassador } from "../../posts/Ambassador"
+import { DynamicHero } from '../../Container/DynamicHero'
+import { Ambassador } from '../../posts/Ambassador'
 
-export default function AmbassadorPage() {
-  return <DynamicHero
-    imageUrl='/late_info.webp'
-    alt='Informacion de la maquina Ambassador'
-    seo='Ambassador'
-  >
-     <Ambassador />
-  </DynamicHero>
+export default function AmbassadorPage () {
+  return (
+    <DynamicHero
+      imageUrl='https://res.cloudinary.com/djc1umong/image/upload/v1667435625/Ambassador_ufms7x.webp'
+      alt='Informacion de la maquina Ambassador'
+      seo='Ambassador'
+    >
+      <Ambassador />
+    </DynamicHero>
+  )
 }
